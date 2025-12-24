831175054c1c517fd4f1da7572388e7e869aefe2
Keshav Pradeep Ramanath <keshavpradeepramanath@gmail.com>
2025-12-24 19:30:23 +0530

    Add files via upload

--- MultiModelDocumentEvalutor/agents/gemini_agent.py
+++ MultiModelDocumentEvalutor/agents/gemini_agent.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 from prompts import EXTRACTION_PROMPT
 
 
-def run_gemini(context, question, model_name="gemini-1.5-pro"):
+def run_gemini(context, question, model_name="gemini-3-pro-preview"):
     """
     Runs document extraction / QnA using Google Gemini.
     """
@@ -17,3 +17,15 @@ def run_gemini(context, question, model_name="gemini-1.5-pro"):
     response = model.generate_content(prompt)
 
     return response.text.strip()
+
+
+def get_supported_gemini_models():
+    genai.configure()
+
+    models = []
+    for m in genai.list_models():
+        if "generateContent" in m.supported_generation_methods:
+            # Remove "models/" prefix
+            print(m.name)
+            models.append(m.name.replace("models/", ""))
+    return models    
