40166c90d388edcd9d00d0b9b29aad3d12c7f88b
Keshav Pradeep Ramanath <keshavpradeepramanath@gmail.com>
2025-12-28 12:17:13 +0530

    Update app.py

--- BaggageClassifier/app.py
+++ BaggageClassifier/app.py
@@ -92,7 +92,7 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     inspect_btn = st.button("🔍 Inspect Items")
 
