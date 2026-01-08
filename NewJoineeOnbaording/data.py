4979a8e114d3f72104973fe6f32b718798acb82b
Keshav Pradeep Ramanath <keshavpradeepramanath@gmail.com>
2026-01-08 19:16:13 +0530

    Add files via upload
    Sample data

---
+++ NewJoineeOnbaording/data.py
@@ -0,0 +1,15 @@
+BUDDIES = [
+    "Arjun (Senior Engineer)",
+    "Meera (Product Manager)",
+    "Rohit (Tech Lead)",
+    "Ananya (HR Partner)"
+]
+
+SOFTWARES = [
+    "Jira",
+    "Slack",
+    "GitHub",
+    "AWS Console"
+]
+
+CONFLUENCE_LINK = "https://confluence.company.com/onboarding"
